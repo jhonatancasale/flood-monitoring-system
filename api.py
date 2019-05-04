f55a52a54ce5ef1560b2e2f5f50cd1053305296f
jhonatancasale <casale.jhon@gmail.com>
2019-05-04 00:23:54 -0300

    Add initial model abstraction

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flood_system_manager import SystemManager
 
 app = Flask(__name__)
+system = SystemManager()
 
 
 @app.route('/api/v1.0/', methods='GET'.split())
@@ -11,56 +13,51 @@ def get_readme():
 
 @app.route('/api/v1.0/status', methods='GET'.split())
 def get_status():
-    return jsonify({'status': 'critical'})
+    return jsonify({'status': system.status})
 
 
 @app.route('/api/v1.0/stations', methods='GET'.split())
 def get_stations():
-    stations = {_id: 'text' for _id in range(10)}
-    return jsonify({'stations': stations})
+    return jsonify({'stations': system.get_stations()})
 
 
 @app.route('/api/v1.0/stations/<int:station_id>', methods='GET'.split())
 def get_station(station_id: int):
-    stations = {_id: 'text' for _id in range(10)}
-    return jsonify({'stations': stations.get(station_id)})
+    return jsonify({'stations': system.get_station(station_id)})
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/minutes/<int:minutes>', methods='GET'.split())
 def get_from_minutes(station_id: int, minutes: int):
-    return jsonify({f'measurements from {station_id}:': minutes})
+    return jsonify({f'data': system.get_from_minutes(station_id, minutes)})
 
 
 @app.route('/api/v1.0/stations/minutes/<int:minutes>', methods='GET'.split())
 def get_all_stations_from_minutes(minutes):
-    return jsonify({'all measurements from:': minutes})
+    return jsonify({'data:': system.get_all_stations_from_minutes(minutes)})
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/date/<string:from_date>', methods='GET'.split())
 def get_from_date(station_id: int, from_date: str):
-    return jsonify({f'measurements from {station_id}:': from_date})
+    return jsonify({f'data:': system.get_from_date(station_id, from_date)})
 
 
 @app.route('/api/v1.0/stations/date/<string:from_date>', methods='GET'.split())
 def get_all_from_date(from_date: str):
-    return jsonify({'all measurements from:': from_date})
+    return jsonify({'data:': system.get_all_from_date(from_date)})
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/date/<string:from_date>/<string:to_date>', methods='GET'.split())
 def get_from_to_date(station_id: int, from_date: str, to_date: str):
     return jsonify(
-        {f'measurements from {station_id}:': from_date, 'until': to_date}
+        {'data': system.get_from_to_date(station_id, from_date, to_date)}
     )
 
 
 @app.route('/api/v1.0/stations/date/<string:from_date>/<string:to_date>', methods='GET'.split())
 def get_all_from_to_date(from_date: str, to_date: str):
-    return jsonify({'all measurements from:': from_date, 'until': to_date})
-
-
-
-
-
+    return jsonify(
+        {'data': system.get_all_from_to_date(from_date, to_date)}
+    )
 
 
 if __name__ == '__main__':
---
+++ flood_system_manager.py
@@ -0,0 +1,69 @@
+from database import Connection
+
+### TODO
+# - [ ] Use a Station class
+
+class SystemManager:
+    conn = Connection()
+
+    @property
+    def status(self) -> str:
+        return self.conn.system_status
+
+    def valid_from_date(self, from_date):
+        return False
+
+    def valid_to_date(self, to_date):
+        return False
+
+    def to_timestamp(date):
+        return ''
+
+    def valid_range_date(self, _from_date, _to_date):
+        from_date = to_timestamp(_from_date)
+        to_date = to_timestamp(_to_date)
+
+        return (
+            self.valid_from_date(from_date) and \
+            self.valid_to_date(to_date) and \
+            from_date < to_date
+        )
+
+    def get_stations(self) -> dict:
+        return self.conn.get_stations()
+
+    def get_station(self, station_id: int) -> dict:
+        return self.conn.get_station(station_id)
+
+    def get_from_minutes(self, station_id: int, minutes: int) -> dict:
+        return {
+            'get_from_minutes': \
+            self.conn.get_from_minutes(station_id, minutes)
+        }
+
+    def get_all_stations_from_minutes(self, minutes):
+        return {
+            'get_all_stations_from_minutes': \
+            self.conn.get_all_stations_from_minutes(minutes)
+        }
+
+    def get_from_date(self, station_id: int, from_date: str):
+        return {'get_from_date': \
+            self.conn.get_from_date(station_id, from_date)
+        }
+
+    def get_all_from_date(self, from_date: str):
+        return {'get_all_from_date': self.conn.get_all_from_date(from_date)}
+
+    def get_from_to_date(self, station_id: int, from_date: str, to_date: str):
+        return {
+            'get_from_to_date': self.conn.get_from_to_date(
+            station_id, from_date, to_date
+        )}
+
+    def get_all_from_to_date(self, from_date: str, to_date: str):
+        return {
+            'get_all_from_to_date': self.conn.get_all_from_to_date(
+                from_date, to_date
+        )}
+
