3f650629afdf56fcb0696938b2c68b625baaa68e
jhonatancasale <casale.jhon@gmail.com>
2019-05-07 02:46:05 -0300

    Start to use Flask-Api

--- core/univesp/src/api.py
+++ core/univesp/src/api.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request, Response
+from flask import request, Response
+from flask_api import FlaskAPI
 
 from core.univesp.src.flood_system_manager import SystemManager
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 system = SystemManager()
 
 
@@ -14,51 +15,47 @@ def get_readme():
 
 @app.route('/api/v1.0/status', methods='GET'.split())
 def get_status():
-    return jsonify({'status': system.status})
+    return {'status': system.status}
 
 
 @app.route('/api/v1.0/stations', methods='GET'.split())
 def get_stations():
-    return jsonify({'stations': system.get_stations()})
+    return {'stations': system.get_stations()}
 
 
 @app.route('/api/v1.0/stations/<int:station_id>', methods='GET'.split())
 def get_station(station_id: int):
-    return jsonify({'station': system.get_station(station_id)})
+    return {'station': system.get_station(station_id)}
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/minutes/<int:minutes>', methods='GET'.split())
 def get_from_minutes(station_id: int, minutes: int):
-    return jsonify({f'data': system.get_from_minutes(station_id, minutes)})
+    return {f'data': system.get_from_minutes(station_id, minutes)}
 
 
 @app.route('/api/v1.0/stations/minutes/<int:minutes>', methods='GET'.split())
 def get_all_stations_from_minutes(minutes):
-    return jsonify({'data:': system.get_all_stations_from_minutes(minutes)})
+    return {'data:': system.get_all_stations_from_minutes(minutes)}
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/date/<string:from_date>', methods='GET'.split())
 def get_from_date(station_id: int, from_date: str):
-    return jsonify({f'data:': system.get_from_date(station_id, from_date)})
+    return {f'data:': system.get_from_date(station_id, from_date)}
 
 
 @app.route('/api/v1.0/stations/date/<string:from_date>', methods='GET'.split())
 def get_all_from_date(from_date: str):
-    return jsonify({'data:': system.get_all_from_date(from_date)})
+    return {'data:': system.get_all_from_date(from_date)}
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/dates/<string:from_date>/<string:to_date>', methods='GET'.split())
 def get_from_to_date(station_id: int, from_date: str, to_date: str):
-    return jsonify(
-        {'data': system.get_from_to_date(station_id, from_date, to_date)}
-    )
+    return {'data': system.get_from_to_date(station_id, from_date, to_date)}
 
 
 @app.route('/api/v1.0/stations/dates/<string:from_date>/<string:to_date>', methods='GET'.split())
 def get_all_from_to_date(from_date: str, to_date: str):
-    return jsonify(
-        {'data': system.get_all_from_to_date(from_date, to_date)}
-    )
+    return {'data': system.get_all_from_to_date(from_date, to_date)}
 
 
 @app.route('/api/v1.0/stations/<int:station_id>/measure', methods='POST'.split())
